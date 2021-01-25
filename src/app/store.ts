3abfda7ea3e70856050630fdac2eb7f272e99df0
HongMuchan <muchang0824@gmail.com>
2021-01-25 12:48:25 +0900

    Firebase

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,10 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import userReducer from '../features/userSlice';
 
+//userSliceから受け取り
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+   user: userReducer,//reducerの任意の名前
   },
 });
 
---
+++ src/features/userSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit';
+import {  RootState } from '../app/store';
+
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    user: { uid: '', photoUrl:'',displayName:''}
+  },
+  reducers: {
+    login: (state,action) => {
+      state.user = action.payload;
+    },
+    logout: state => {
+      state.user = {uid:'', photoUrl:'', displayName:''}
+    },
+  
+  },
+});
+
+export const {login, logout} = userSlice.actions;
+
+
+//ユーザのstateを返す関数
+//最後のuserはreducerの任意の名前と一致させる
+export const selectUser = (state: RootState) => state.user.user;
+
+
+//storeで読み込ませる
+export default userSlice.reducer;
