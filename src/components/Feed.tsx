da3653221304f6c4324f956fd4fb1c81903af123
HongMuchan <muchang0824@gmail.com>
2021-01-25 15:06:54 +0900

    [ADD]ログイン後画面の実装

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { auth } from '../firebase';
+  
 
 const Feed = () => {
   return (
     <div>
       <p>aaa</p>
+      <button
+        onClick={()=>auth.signOut()}
+      >ログアウト</button>
     </div>
   )
 }
